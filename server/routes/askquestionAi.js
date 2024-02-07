1ef65e2463b6606965a9bfa45f3cf2d31e781c3a
siku97 <subhakantadas452@gmail.com>
2024-02-07 17:07:33 +0530

    first commit

---
+++ server/routes/askquestionAi.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import {QuestionAi} from '../controllers/questions.js'
+
+
+const router = express.Router()
+
+router.post('/question',QuestionAi)
+
+export default router
